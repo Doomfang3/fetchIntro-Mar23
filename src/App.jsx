3dd74dc4a74f107da5f2b46425d54f76dddd1918
B4n3l1ng <114490314+B4n3l1ng@users.noreply.github.com>
2023-05-12 10:22:14 +0100

    axios version

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,49 @@
-import { useEffect, useState } from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [launches, setLaunches] = useState([])
-  const [needRefresh, setNeedRefresh] = useState(false)
+  const [launches, setLaunches] = useState([]);
+  const [needRefresh, setNeedRefresh] = useState(false);
 
   const fetchData = async () => {
-    /* fetch('https://api.spacexdata.com/v4/launches')
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        console.log('Parsed response: ', data)
-      })
-      .catch(err => console.log(err)) */
     try {
-      const response = await fetch('https://api.spacexdata.com/v4/launches')
-      const parsed = await response.json()
-      console.log(parsed)
-      setLaunches(parsed)
-      setNeedRefresh(false)
+      const response = await axios.get(
+        "https://api.spacexdata.com/v4/launches"
+      );
+      console.log(response.status);
+      if (response.status === 200) {
+        setLaunches(response.data);
+        setNeedRefresh(false);
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (needRefresh) {
-      fetchData()
+      fetchData();
     }
-  }, [needRefresh])
+  }, [needRefresh]);
 
   return (
     <>
-      <button type='button' onClick={() => setNeedRefresh(true)} disabled={needRefresh}>
+      <button
+        type="button"
+        onClick={() => setNeedRefresh(true)}
+        disabled={needRefresh}
+      >
         Refresh
       </button>
-      {launches.map(launch => (
-        <h2>Launch Succeeded: {launch.success ? 'Yes' : 'No'}</h2>
+      {launches.map((launch) => (
+        <h2>Launch Succeeded: {launch.success ? "Yes" : "No"}</h2>
       ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
